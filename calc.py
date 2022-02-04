24b6bee51487d4bdfb01529de1ab56ca280f31fb
Gilad Zur <Gilad.zur90@gmail.com>
2022-02-04 10:20:29 +0200

    commit_test1

---
+++ calc.py
@@ -0,0 +1,24 @@
+"""
+This is module to help ypu add,divide,subtract and multuply numbers
+"""
+
+def add(argument_a, argument_b)
+"""
+This function add two numbers
+"""
+	return argument_a+argument_b
+def div(argument_a, argument_b)
+"""
+This function divide two numbers, a on b
+"""
+	return argument_A/argument_b
+def mul(argument_a, argument_b)
+"""
+This function mulltiply thw two numbers
+"""
+	return argumet_a*argument_b
+def sub(argument_a,argument_b)
+"""
+This function subtract the two numbers, a on b
+"""
+	return argument_a-argument_b
