e14683dca7243c2f209b954148acbbea6db68884
Gilad Zur <Gilad.zur90@gmail.com>
2022-02-04 10:53:32 +0200

    commit2_tests

--- calc.py
+++ calc.py
@@ -1,24 +1,3 @@
-"""
-This is module to help ypu add,divide,subtract and multuply numbers
-"""
 
-def add(argument_a, argument_b)
-"""
-This function add two numbers
-"""
-	return argument_a+argument_b
-def div(argument_a, argument_b)
-"""
-This function divide two numbers, a on b
-"""
-	return argument_A/argument_b
-def mul(argument_a, argument_b)
-"""
-This function mulltiply thw two numbers
-"""
-	return argumet_a*argument_b
-def sub(argument_a,argument_b)
-"""
-This function subtract the two numbers, a on b
-"""
-	return argument_a-argument_b
+def add(argument_a, argument_b):
+	return argument_a + argument_b
--- project_tests/test_basic.py
+++ project_tests/test_basic.py
@@ -1,23 +1,8 @@
 import unittest
-from calc import calx
+from calc import add
 
 class TestBasic(unittest.TestCase):
-print("Add func check : ")
-def test_add(self):
-	self.assertEqual(add(2,2), 4)
-def test_add(self):
-	self.assertEqual(add(4,4), 8)
-print("Mul function check :")
-def test_mul(self):
-	self.assertEqual(mul(2,2), 4)
-def test_mul(self):
-	self.assertEqual(mul(4,4), 16)
-print("Sub function check :")
-def test_sub(self):
-	self.assertEqual(sub(2,2), 0)
-def test_sub(self):
-	self.assertEqual(sub(4,4), 0)
-print("Div function check :")
-def test_div(self):
-	self.assertEqual(div(2,2), 1)
-	self.assertEqual(siv(4,4), 1)
+	def test_add(self):
+		self.assertEqual(add(2,2), 4)
+	def test_add(self):
+		self.assertEqual(add(4,4), 8)
